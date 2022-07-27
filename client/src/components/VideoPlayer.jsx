5c08f6801b0eb9cc584d1fb91aadef015bdaad78
skc52 <99289560+skc52@users.noreply.github.com>
2022-07-27 03:19:27 -0400

    pushing my code into the guthub for thr first time for this project

---
+++ client/src/components/VideoPlayer.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import {makeStyles} from '@mui/styles'
+
+import {Grid, Typography, Paper} from "@mui/material"
+import { SocketContext  } from '../SocketContext.js';
+import { useContext } from 'react';
+const useStyles = makeStyles((theme) => ({
+    video: {
+      width: '550px',
+      
+    },
+    gridContainer: {
+      justifyContent: 'center',
+      
+    },
+    paper: {
+      padding: '10px',
+      border: '2px solid black',
+      margin: '10px',
+    },
+  }));
+const VideoPlayer = () => {
+    const {
+        call,
+        callAccepted, 
+        myVideo,
+        userVideo,
+        callEnded,
+        stream,
+        name
+
+    } = useContext(SocketContext);
+    const classes = useStyles();
+  return (
+    <Grid container className={classes.gridContainer}>
+       
+        {
+            stream && (
+                <Paper className={classes.paper}>
+                    <Grid item xs = {12} md = {6}>
+                        <Typography variant='h5' gutterBottom>{name||'Name'}</Typography>
+                        <video playsInline muted ref = {myVideo} autoPlay className={classes.video } />
+                    </Grid>
+                </Paper>
+               
+               
+            )   
+        }
+        {
+            callAccepted && !callEnded && (
+                <Paper className={classes.paper}>
+                    <Grid item xs = {12} md = {6}>
+                        <Typography variant='h5' gutterBottom>{call.name||'Name'}</Typography>
+                        <video playsInline  ref = {userVideo} autoPlay className={classes.video } />
+                    </Grid>
+                </Paper>
+            )
+        }
+         
+        
+    </Grid>
+  )
+}
+
+export default VideoPlayer
---
+++ client/src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+import App from './App.js';
+import { ContextProvider } from "./SocketContext.js";
+import './style.css';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+root.render(
+    <React.StrictMode>
+        <ContextProvider>
+            <App/>
+        </ContextProvider>
+    </React.StrictMode>
+    
+    
+
+);
